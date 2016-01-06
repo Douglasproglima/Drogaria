4e39ed23207badbbf7f88db52a8e8873f1a0786b
Douglasproglima <douglasproglima@gmail.com>
2016-01-06 07:20:29 -0200

    Aula 145 - Entidade Estado e Cidade Relacionamento ManyToOne

---
+++ src/main/java/br/pro/delfino/drogaria/domain/Fabricante.java
@@ -0,0 +1,17 @@
+package br.pro.delfino.drogaria.domain;
+
+import javax.persistence.Entity;
+
+@SuppressWarnings("serial")
+@Entity
+public class Fabricante extends GenericDomain{
+	private String descricao;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
