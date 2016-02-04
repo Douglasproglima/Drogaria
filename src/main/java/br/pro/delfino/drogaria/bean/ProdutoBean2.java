b4186535fa4d999579e8715842d8d65a50b3c6ee
Douglasproglima <douglasproglima@gmail.com>
2016-02-04 07:37:06 -0200

    Aula 256 - Pesquisa Avançada 1

---
+++ src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean2.java
@@ -0,0 +1,30 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.pro.delfino.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class ProdutoBean2 implements Serializable{
+	private Produto produto;
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	//Para chamar o método quando a tela estiver sendo carregada basta criar o @PostConstruct
+	@PostConstruct
+	public void novo(){
+		produto = new Produto();
+	}
+}
