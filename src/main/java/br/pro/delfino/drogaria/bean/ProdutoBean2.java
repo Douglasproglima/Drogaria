228d8fd9f86b8df808501cedcd6e178bd660b07e
Douglas Lima <douglasproglima@gmail.com>
2016-02-10 14:21:59 -0200

    Aula 264 - Busca de Produtos - Parte 3 Filtro Extra

--- src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean2.java
+++ src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean2.java
@@ -5,9 +5,6 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
-import org.hibernate.Criteria;
-import org.hibernate.Session;
 import org.omnifaces.util.Messages;
 
 import br.pro.delfino.drogaria.dao.ProdutoDAO;
@@ -18,6 +15,7 @@ import br.pro.delfino.drogaria.domain.Produto;
 @ViewScoped
 public class ProdutoBean2 implements Serializable{
 	private Produto produto;
+	private Boolean exibePainelDados;
 	
 	public Produto getProduto() {
 		return produto;
@@ -27,10 +25,19 @@ public class ProdutoBean2 implements Serializable{
 		this.produto = produto;
 	}
 	
+	public Boolean getExibePainelDados() {
+		return exibePainelDados;
+	}
+	
+	public void setExibePainelDados(Boolean exibePainelDados) {
+		this.exibePainelDados = exibePainelDados;
+	}
+	
 	//Para chamar o método quando a tela estiver sendo carregada basta criar o @PostConstruct
 	@PostConstruct
 	public void novo(){
 		produto = new Produto();
+		exibePainelDados = false;
 	}
 	
 	public void buscar(){
@@ -39,9 +46,11 @@ public class ProdutoBean2 implements Serializable{
 			Produto resultado =  produtoDAO.buscar(produto.getCodigo());
 			
 			if (resultado ==  null) {
-				Messages.addGlobalWarn("Não foi encontrado o registro informado.");
+				exibePainelDados = false;
+				Messages.addGlobalWarn("Não foi encontrado o registro de código: "+ produto.getCodigo());
 			} else {
-				produto = resultado;
+				exibePainelDados = true;
+				produto = resultado;				
 			}
 			
 		} catch (RuntimeException erro) {
