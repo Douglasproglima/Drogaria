94f04a2773a0e0594eb2b3d8df2fec73fd155e7d
Douglas Lima <douglasproglima@gmail.com>
2016-02-10 15:01:25 -0200

    Aula 266 - Pesquisa Extra e Histórico de Produtos Parte 2

--- src/main/java/br/pro/delfino/drogaria/bean/HistoricoBean.java
+++ src/main/java/br/pro/delfino/drogaria/bean/HistoricoBean.java
@@ -1,21 +1,33 @@
 package br.pro.delfino.drogaria.bean;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
 
+import br.pro.delfino.drogaria.dao.HistoricoDAO;
 import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.Historico;
 import br.pro.delfino.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class HistoricoBean implements Serializable{
+	private Historico historico;
 	private Produto produto;
 	private Boolean exibePainelDados;
+	
+	public Historico getHistorico() {
+		return historico;
+	}
+	
+	public void setHistorico(Historico historico) {
+		this.historico = historico;
+	}
 	
 	public Produto getProduto() {
 		return produto;
@@ -36,6 +48,7 @@ public class HistoricoBean implements Serializable{
 	//Para chamar o método quando a tela estiver sendo carregada basta criar o @PostConstruct
 	@PostConstruct
 	public void novo(){
+		historico =  new Historico();
 		produto = new Produto();
 		exibePainelDados = false;
 	}
@@ -59,4 +72,18 @@ public class HistoricoBean implements Serializable{
 		}
 	}
 	
+	public void salvar(){
+		try {
+			historico.setDataHorario(new Date());
+			historico.setProduto(produto);
+			
+			HistoricoDAO historicoDAO = new HistoricoDAO();
+			historicoDAO.salvar(historico);
+			
+			Messages.addGlobalInfo("Registro salvo com sucesso.");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Erro ao salvar o registro. Erro: "+ erro);
+			erro.printStackTrace();
+		}
+	}
 }
