1b37491d90e64776305bdbab4998b571f5b538b5
Douglas Lima <douglasproglima@gmail.com>
2016-02-11 20:54:51 -0200

    Aula 269 e 270 - Fluxo de Caixa

---
+++ src/main/java/br/pro/delfino/drogaria/domain/Movimentacao.java
@@ -0,0 +1,44 @@
+package br.pro.delfino.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@SuppressWarnings("serial")
+@Entity
+public class Movimentacao extends GenericDomain{
+	private Date horario;
+	private BigDecimal valorMovimentacao;
+	
+	@ManyToOne
+	@JoinColumn(name = "codCaixa", nullable = false, foreignKey = @ForeignKey(name = "FK_MovimentacaoXCaixa") )
+	private Caixa caixa;
+
+	public Date getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+
+	public BigDecimal getValorMovimentacao() {
+		return valorMovimentacao;
+	}
+
+	public void setValorMovimentacao(BigDecimal valorMovimentacao) {
+		this.valorMovimentacao = valorMovimentacao;
+	}
+
+	public Caixa getCaixa() {
+		return caixa;
+	}
+
+	public void setCaixa(Caixa caixa) {
+		this.caixa = caixa;
+	}
+}
---
+++ src/test/java/br/pro/delfino/drogaria/dao/CaixaDAOTest.java
@@ -0,0 +1,34 @@
+package br.pro.delfino.drogaria.dao;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.pro.delfino.drogaria.domain.Caixa;
+
+public class CaixaDAOTest {
+	@Test
+	@Ignore
+	public void salvar() throws ParseException{
+		Caixa caixa =  new Caixa();
+		caixa.setDataAbertura(new SimpleDateFormat("dd/MM/yyyy").parse("11/02/2016"));
+		caixa.setValorAbertura(new BigDecimal(100.50));
+		
+		CaixaDAO caixaDAO = new CaixaDAO();
+		caixaDAO.merge(caixa);
+	}
+	
+	@Test
+	public void buscar() throws ParseException{
+		CaixaDAO caixaDAO = new CaixaDAO();
+		Caixa caixa = caixaDAO.buscar(new SimpleDateFormat("dd/MM/yyyy").parse("12/02/2016"));
+		System.out.println("Caixa existe: "+caixa);
+		
+		//Geralmente usado em teste automatizado
+		Assert.assertNull(caixa);
+	}
+}
